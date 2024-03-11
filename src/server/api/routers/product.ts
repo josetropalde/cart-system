ea9c0a444e3cc9609b34c61156576bce79f42722
José Henrique <josehenrique.tropalde@gmail.com>
2024-03-10 21:54:10 -0300

    feat: add product schema

---
+++ src/server/api/routers/product.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const productRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.product.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+  }),
+});
